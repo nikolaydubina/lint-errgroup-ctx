feb5d18897a068e6b1c735850a2417c11e84b728
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2025-08-10 14:23:57 +0800

    fix

--- analysis/errgroupctx/analyzer_test.go
+++ analysis/errgroupctx/analyzer_test.go
@@ -3,7 +3,7 @@ package errgroupctx_test
 import (
 	"testing"
 
-	"github.com/ndx-technologies/lint-errgroup-ctx/analysis/errgroupctx"
+	"github.com/nikolaydubina/lint-errgroup-ctx/analysis/errgroupctx"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/ndx-technologies/lint-errgroup-ctx/analysis/errgroupctx"
+	"github.com/nikolaydubina/lint-errgroup-ctx/analysis/errgroupctx"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
